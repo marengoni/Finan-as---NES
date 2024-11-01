b2903fbd946037db9391e393fa462e1224910ada
Gabriel Henrique Marengoni <marengoni.gabriel@escola.pr.gov.br>
2024-11-01 17:41:23 -0300

    Código atualizado

--- finances/transactions.py
+++ finances/transactions.py
@@ -7,10 +7,28 @@ CATEGORIES = {
 }
 
 class Transaction:
-    def _init_(self, amount: float, category: int, description: str = '') -> None:
+    def __init__(self, amount: float, category: int, description: str = '') -> None:
         if category not in CATEGORIES.keys():
             raise ValueError('Categoria inválida')
         self.amount = amount
         self.date = datetime.now()
         self.category = category
         self.description = description
+
+    def update(
+        self,
+        amount: float | None = None,
+        category: int | None = None,
+        description: str | None = None,
+        date: datetime | None = None,
+    ) -> None:
+        if amount is not None:
+            self.amount = amount
+        if category is not None:
+            if category not in CATEGORIES.keys():
+                raise ValueError("Categoria inválida")
+            self.category = category
+        if description is not None:
+            self.description = description
+        if date is not None:
+            self.date = date
